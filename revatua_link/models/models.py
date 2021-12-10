aaa3bbbda7aa6351cae24f51be92c8088bf13b70
Tepava <mehdi@pacific-erp.com>
2021-12-10 00:12:04 +0000

    initialisation api

---
+++ revatua_link/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class revatua_link(models.Model):
+#     _name = 'revatua_link.revatua_link'
+#     _description = 'revatua_link.revatua_link'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
